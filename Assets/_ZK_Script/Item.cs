9be1a012cc070b92abb0f53926a93cded3ee2cda
evangerlion <501295105@qq.com>
2017-11-22 23:23:38 +0800

    v0.01 武器脚本上传
    武器脚本上传

---
+++ Assets/_ZK_Script/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DimensionCollapse
+{
+    public abstract class Item : MonoBehaviour
+    {
+        String Name; //物品名称
+        int ID; //物品ID
+        bool Picked; //标识符，物品被捡起为true，没有被捡起为false
+    }
+}
---
+++ Assets/_ZK_Script/RangedWeapon.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DimensionCollapse;
+
+namespace DimensionCollapse
+{
+    public class RangedWeapon : DimensionCollapse.Weapon
+    {
+        float InitialV; //发射子弹的初始速度
+        float Interval; //武器的射速
+        int CurrentChanger; //武器正在使用的子弹数量
+        int CurrentChangerCapacity; //武器正在使用的子弹最大容量
+        int AlternativeCharger; //武器备用子弹数量
+        int AlternativeChargerCapacity; //武器备用子弹最大容量
+
+        public override void Attack()
+        {
+            GameObject fireBullet = GameObject.FindGameObjectWithTag("FireBullet");
+            if (fireBullet != null)
+            {
+                fireBullet.transform.position = this.transform.TransformPoint(0, 0, 3);
+                Vector3 force = fireBullet.transform.TransformDirection(0, 2, 10);
+
+                //给这个火焰子弹添加刚体
+                if (fireBullet.GetComponent<Rigidbody>() == null)
+                {
+                    fireBullet.gameObject.AddComponent<Rigidbody>();
+                }
+                fireBullet.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
+            }
+            else
+            {
+                Debug.Log("找不到火焰子弹");
+            }
+        }
+
+    }
+}
