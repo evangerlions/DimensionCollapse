b94cc402207ec7b3155b6010ca8b291e681c51bd
evangerlion <501295105@qq.com>
2017-12-06 16:55:17 +0800

    fix: 修复速度过快子弹穿过物体的Bug
    Changes：
    1. 给m4子弹加上射线检测来，防止子弹过快穿越物体。已经进行测试，现在会正确击中子弹前面第一个物体，在高速情况下也不会穿透，但是子弹成抛物线并且接近最高点时射线检测会检测到自己？原因不明，解决方式加入排除自己的判断。
    2. 调整m4子弹碰撞体为Box collider，节省开销。测试发现对于一个小物体，初速度为50的以上有可能发生穿透，所以设定为初速度50以上才添加射线，射线发射原则是从上一帧位置发射一条到这一帧位置的射线。
    3. 更改武器初速度算法，现在输进去多少初速度，子弹就是多少初速度（不受子弹质量影响）,已进行精确测试。
    4. 优化了代码，现在生成的子弹会有一个父物体了，从而在Hierarchy中层次更加分明了。
    5. Wiley发射特效没了？重新添加了一下。
    6. 有关射线模拟添加的冲量，和实际物理引擎加的冲量有10%到20%的误差，具体情况不明，准备维持现状。

---
+++ Assets/_ZK_Project/_ZK_Script/Temp_Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DimensionCollapse;
+public class Temp_Test : MonoBehaviour
+{
+    //  private Vector3 orginal;
+    // Use this for initialization
+    void Start()
+    {
+        //        orginal = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
+    void Update()
+    {
+      
+    }
+}
